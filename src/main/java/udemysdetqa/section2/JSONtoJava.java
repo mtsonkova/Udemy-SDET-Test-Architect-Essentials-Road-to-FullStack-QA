1164242536af9f63cd3f4a9b1a6195936c74e54b
mtsonkova <mtsonkova950@gmail.com>
2024-09-01 23:23:07 +0300

    In progress

--- src/main/java/udemysdetqa/section2/JSONtoJava.java
+++ src/main/java/udemysdetqa/section2/JSONtoJava.java
@@ -2,14 +2,27 @@ package udemysdetqa.section2;
 
 import com.mysql.cj.jdbc.Driver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JSONtoJava {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = null;
-        DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "rootpass");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/business", "root", "rootpass");
+
+        //object of statement class will help us to execute queries;
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from CustomerInfo where purchasedDate=CURDATE() and Location ='Europe'");
+
+
+        while(resultSet.next()) {
+            System.out.println(resultSet.getString(1));
+            System.out.println(resultSet.getString(2));
+            System.out.println(resultSet.getInt(3));
+            System.out.println(resultSet.getString(4));
+        }
+
+        connection.close();
     }
 }
