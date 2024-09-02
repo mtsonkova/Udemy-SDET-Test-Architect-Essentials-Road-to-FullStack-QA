98368ad960970b719c9157bc9924d4ebc2688081
mtsonkova <mtsonkova950@gmail.com>
2024-09-02 11:23:05 +0300

    DB query results converted to POJO classes and then to JSON objects

---
+++ src/main/java/udemysdetqa/section2/CustomerDetails.java
@@ -0,0 +1,58 @@
+package udemysdetqa.section2;
+
+public class CustomerDetails {
+    private int customerNumber;
+    private String customerName;
+    private String contactLastName;
+    private String contactFirstName;
+    private String city;
+    private String country;
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+
+    public String getContactFirstName() {
+        return contactFirstName;
+    }
+
+    public void setContactFirstName(String contactFirstName) {
+        this.contactFirstName = contactFirstName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
--- src/main/java/udemysdetqa/section2/JSONtoJava.java
+++ src/main/java/udemysdetqa/section2/JSONtoJava.java
@@ -1,27 +1,41 @@
 package udemysdetqa.section2;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.xdevapi.SqlStatement;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 
 public class JSONtoJava {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = null;
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/business", "root", "rootpass");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root", "rootpass");
 
         //object of statement class will help us to execute queries;
-
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from CustomerInfo where purchasedDate=CURDATE() and Location ='Europe'");
-
+        ResultSet resultSet = statement.executeQuery("select customerNumber, customerName, contactLastName, contactFirstName, city, country from customers limit 5;");
+        CustomerDetails customerDetails;
+        int counter = 0;
 
         while(resultSet.next()) {
-            System.out.println(resultSet.getString(1));
-            System.out.println(resultSet.getString(2));
-            System.out.println(resultSet.getInt(3));
-            System.out.println(resultSet.getString(4));
+            customerDetails = new CustomerDetails();
+            customerDetails.setCustomerNumber(resultSet.getInt(1)); //customerNumber
+            customerDetails.setCustomerName(resultSet.getString(2)); //customerName
+            customerDetails.setContactLastName(resultSet.getString(3)); //contactLastName
+            customerDetails.setContactFirstName(resultSet.getString(4)); //contactFirstName
+            customerDetails.setCity(resultSet.getString(5)); //city
+            customerDetails.setCountry(resultSet.getString(6)); //country
+            counter++;
+
+            String jsonPath = System.getProperty("user.dir") + "\\JsonFiles\\customerInfo" + counter + ".json";
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File(jsonPath), customerDetails);
         }
+
 
         connection.close();
     }
