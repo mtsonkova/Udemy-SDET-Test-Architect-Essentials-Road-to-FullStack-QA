d4b4ec426513391af314645509f73b30259e4b08
mtsonkova <mtsonkova950@gmail.com>
2024-09-05 00:07:01 +0300

    Issue fixed by creating new docker container with this line of code docker run -d -p 4444:4444 -p 7900:7900 --shm-size=2g selenium/standalone-chrome:latest. Container is started with cmd.  Also test case updated by adding @Before and @Test annotations and bringing JUnit5 into the picture

--- src/main/java/udemysdetqa/dockerandselenium/ChromeStandaloneTest.java
+++ src/main/java/udemysdetqa/dockerandselenium/ChromeStandaloneTest.java
@@ -1,7 +1,8 @@
 package udemysdetqa.dockerandselenium;
 
-
-import org.openqa.selenium.WebDriver;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -10,7 +11,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ChromeStandaloneTest {
-    public static void main(String[] args) throws MalformedURLException {
+    // Test run successfully with this code
+    //docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-chrome:latest
+
+    private RemoteWebDriver driver;
+
+    @Before
+    public void init() throws MalformedURLException {
         // Define the URL of the Selenium Grid hub
         URL hubUrl = new URL("http://localhost:4444/wd/hub"); // Replace with your grid URL
         //URL hubUrl = new URL("https://172.17.0.2:4444/wd/hub");
@@ -22,22 +29,18 @@ public class ChromeStandaloneTest {
         // Create DesiredCapabilities instance for Chrome
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-
-        // Initialize RemoteWebDriver with hub URL and capabilities
-        RemoteWebDriver driver = new RemoteWebDriver(hubUrl, capabilities);
-
+        driver = new RemoteWebDriver(hubUrl, capabilities);
+    }
+    @Test
+    public void getTitle() {
         // Navigate to a website
         driver.get("https://guru99.com");
         String title = driver.getTitle();
-        System.out.println(title);
+        Assert.assertTrue(title.contains("Guru99"));
 
-        // Perform actions and validate the behavior
-
-        // Close the browser
         driver.quit();
 
-        //TODO Solve error on runtime. Chrome driver crashes.
-        //Try to run the code after updating the docker image
+
     }
 
 
