c41927994d3560d656483cab66b2f0d685074e59
mtsonkova <mtsonkova950@gmail.com>
2024-09-04 23:43:43 +0300

    Attempt to fix the error by updating the docker image file and rerun the container

--- src/main/java/udemysdetqa/dockerandselenium/ChromeStandaloneTest.java
+++ src/main/java/udemysdetqa/dockerandselenium/ChromeStandaloneTest.java
@@ -1,5 +1,7 @@
 package udemysdetqa.dockerandselenium;
 
+
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -9,23 +11,34 @@ import java.net.URL;
 
 public class ChromeStandaloneTest {
     public static void main(String[] args) throws MalformedURLException {
-        //RemoteWebDriver Class takes 2 paramenters
-        //the url with the port where your tests are running
-        //desired capabilities
+        // Define the URL of the Selenium Grid hub
+        URL hubUrl = new URL("http://localhost:4444/wd/hub"); // Replace with your grid URL
+        //URL hubUrl = new URL("https://172.17.0.2:4444/wd/hub");
+        // Create ChromeOptions instance
         ChromeOptions chromeOptions = new ChromeOptions();
-        //TODO Solve error Could not start a new session. Response code 500. Driver cannot start
-        try {
-            URL url = new URL("http://localhost:4444/wd/hub"); //wd stands for webdriver
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--start-maximized");
 
-            RemoteWebDriver driver = new RemoteWebDriver(url, chromeOptions);
-            driver.get("https://www.guru99.com/");
-            String title = driver.getTitle();
-            System.out.println(title);
+        // Create DesiredCapabilities instance for Chrome
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 
-            driver.quit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Initialize RemoteWebDriver with hub URL and capabilities
+        RemoteWebDriver driver = new RemoteWebDriver(hubUrl, capabilities);
+
+        // Navigate to a website
+        driver.get("https://guru99.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+
+        // Perform actions and validate the behavior
+
+        // Close the browser
+        driver.quit();
+
+        //TODO Solve error on runtime. Chrome driver crashes.
+        //Try to run the code after updating the docker image
     }
+
 
 }
